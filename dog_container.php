0fe1167b3fb2839f4032cff81760da375af308ed
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2024-12-04 09:27:13 +0100

    dog_container debugged

--- dog_container.php
+++ dog_container.php
@@ -3,13 +3,13 @@ class Dog_container {
   private string $app;
   private $dog_location;
 
-/*   function __construct($value) {
+  function __construct($value) {
     if(function_exists('get_properties')) {
       $this->app=$value;
     } else {
       exit;
     }
-  } */
+  }
 
   public function set_app($value) {
     $this->app=$value;
@@ -17,22 +17,24 @@ class Dog_container {
 
   public function get_dog_application() {
     $xmlDoc=new DOMDocument();
-    if (file_exists('./xml-files/dog_applications.xml')) {
-      $xmlDoc->load('./xml-files/dog_applications.xml');
-      $searchNode=$xmlDoc->getElementsByTagName('type');
-
-      foreach($searchNode as $searchNode) {
-        
+    if (file_exists("./xml-files/dog_applications.xml")) {
+      $xmlDoc->load("./xml-files/dog_applications.xml");
+      $typeNodes=$xmlDoc->getElementsByTagName("type");
+      foreach($typeNodes as $searchNodes) {
+        $id=$searchNodes->getAttribute('ID');
+        if($id==$this->app) {
+          $location=$searchNodes->getElementsByTagName('location');
+          $file=$location->item(0)->nodeValue;
+          return $file;
+        }
       }
-      
-
     }
   }
 }
 
-$dog_cont = new Dog_container();
+$dog_cont = new Dog_container('breeds');
 
-$dog_cont->get_dog_application();
+print $dog_cont->get_dog_application();
 
 
 ?>
